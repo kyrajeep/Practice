378606f8968fabd1dce3c28443a4d6a4801f92a8
Jee Won (Kyra) Park <jee_won_park@alumni.brown.edu>
2024-02-08 14:13:14 -0500

    Save time with a pre-filled table
    and iterate through the size of the grid to not exceed it.

--- count_islands.py
+++ count_islands.py
@@ -1,27 +1,25 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        result = 0 
-        rows, columns = len(grid), len(grid[0])
-        q = collections.deque()
-        visited = []
-        directions = [[1,0], [-1,0], [0,1], [0,-1]]
-        def bfs(row, column):
-        # add the first element
-            current = [row, column]
-            q.append(current)
-            visited.append(current)
-            while q:
-                current = q.popleft()
-                for i in directions:
-                # add the adjacent elements
-                    dir = current + i
-                    if dir not in visited:
-                        q.append(dir)
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0   
+        count = 0
+        check = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] =='1' and check[i][j]== False:
+                    count += 1
+                    self.search(grid,check,i,j)
+        return count       
+    def search(self,grid,check,i,j):
+        qu = deque([(i,j)])
+        while qu:
+            i, j = qu.popleft()
+            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]=='1' and check[i][j]==False:
+                check[i][j] = True
+                qu.extend([(i-1,j),(i+1,j),(i,j-1),(i,j+1)])
                     
-                    
-            return visited, result
-        for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] == '1' and [i,j] not in visited: 
-                    bfs(i,j)
-        return result
